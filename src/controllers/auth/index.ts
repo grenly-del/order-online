334406ac7482afacc343cc28611936f7461d8c6e
grenly-del <ardiand400@gmail.com>
2024-12-10 23:42:09 +0800

    menyelesaikan otp email

--- src/controllers/auth/index.ts
+++ src/controllers/auth/index.ts
@@ -5,6 +5,7 @@ import { userSchema, userType } from "../../config/validations/users"
 import { HTTP_STATUS_CODE } from "../../config/httpsCode"
 import { GenereteID } from "../../utils/genereteID"
 import { sendEmail } from "../../config/email"
+import bcrypt from 'bcrypt'
 
 
 export const RegisPost = async (req:Request, res:Response) => {
@@ -18,12 +19,13 @@ export const RegisPost = async (req:Request, res:Response) => {
            // Lanjutkan dengan membuat ID_Pengguna
            const id = await GenereteID("US")
           
+           const newPasswordHash = bcrypt.hashSync(result.data.password, 10)
             // Simpan di database data user
            const newUser = await new UsersModel({
                 id_pengguna: id,
                 nama_pengguna: result.data.nama_pengguna,
                 email: result.data.email,
-                password: result.data.password,
+                password: newPasswordHash,
                 role: result.data.role,
                 status: false
             }).save()
@@ -87,14 +89,39 @@ export const GetOTPEmail = async (req:Request, res:Response) => {
         const expireDate = new Date()
         expireDate.setMinutes(expireDate.getMinutes()+1) // 1 Menit Expire OTP
         // Simpan OTP sesuai dengan ID Pengguna
+
         await new OTPEmailModel({
             otp_code: otpRandom,
             user_id: Users._id,
             expiresAt: expireDate
         }).save()
+
+        res.status(HTTP_STATUS_CODE.OK).json({message: 'Berhasil'})
     }
     
 
+}
+
+export const CekOTPEmail = async (req:Request, res:Response) => {
+    try{
+        const data = req.body
+        const otp_email = Number(data.otp_email)
+        const UserOTPValid = await OTPEmailModel.findOne({otp_code: otp_email})
+        if(!UserOTPValid) {
+            res.status(HTTP_STATUS_CODE.NOT_FOUND).json({
+                message: 'Gagal',
+                data: "OTP tidak valid"
+            })
+        }else {
+            const newUsers = await UsersModel.findOneAndUpdate({_id: UserOTPValid?.user_id}, {$set : {status: true}},  { new: true })
+            console.log(newUsers)
+            await OTPEmailModel.findOneAndDelete({user_id: newUsers?._id})
+            res.send('berhasil')
+        }
+    }
+    catch(err) {
+        res.status(HTTP_STATUS_CODE.BAD_REQUEST).json({message: 'gagal', error: err})
+    }
 }
 
 export const LoginGet = (req:Request, res:Response) => {
--- src/routes/authRoute/index.ts
+++ src/routes/authRoute/index.ts
@@ -1,11 +1,12 @@
 import {Router} from 'express'
 import RegisRoute from './regis'
 import LoginRoute from './login'
-import { GetOTPEmail } from '../../controllers/auth'
+import { CekOTPEmail, GetOTPEmail } from '../../controllers/auth'
 
 const route = Router()
 
 route.post('/getotp', GetOTPEmail)
+route.post('/cekotp', CekOTPEmail)
 
 route.use('/regis', RegisRoute)
 route.use('/login', LoginRoute)
